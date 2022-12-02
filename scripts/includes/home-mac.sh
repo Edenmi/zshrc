f626639facfc3de79dfb6bf92dc81bd1f31493f0
ZH <zh.eden@icloud.com>
2022-12-03 01:55:47 +0800

    Update home-mac.sh

--- scripts/includes/home-mac.sh
+++ scripts/includes/home-mac.sh
@@ -7,8 +7,8 @@ if [[ $OSTYPE == 'darwin'* ]] && [[ $HOST == 'HyDEV' ]]; then
     # Paths
     export DOTNET_ROOT="/usr/local/opt/dotnet/libexec"
     # export PATH="/Volumes/MacData/SageMath:$PATH"
-    export PATH="$PATH:/Users/hykilpikonna/.gem/ruby/2.6.0/bin" # https://stackoverflow.com/a/53388305/7346633
-    export PATH="/Users/hykilpikonna/Resources/flutter/bin:$PATH"
+    export PATH="$PATH:/Users/zh/.gem/ruby/2.6.0/bin" # https://stackoverflow.com/a/53388305/7346633
+    export PATH="/Users/zh/Resources/flutter/bin:$PATH"
     export ANDROID_HOME="~/Resources/AndroidSDK"
     export PATH="${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
     export NDK_HOME="/Volumes/MacData/Resources/android-ndk-r21d"
@@ -16,7 +16,21 @@ if [[ $OSTYPE == 'darwin'* ]] && [[ $HOST == 'HyDEV' ]]; then
     export PATH="/usr/local/opt/node@14/bin:$PATH"
 
     # Use Python3.9 by default
-    export PATH="/usr/local/opt/python@3.8/bin:$PATH"
-    export PATH="/usr/local/opt/python@3.10/bin:$PATH"
-    export PATH="/usr/local/opt/python@3.9/bin:$PATH"
+    
+fi
+
+SCR="$HOME/zshrc/scripts"
+. $SCR/zshrc.sh
+
+export PATH=/Users/zh/Library/Python/3.9/bin:$PATH
+export PATH=/usr/local/bin:$PATH
+export PATH=/Users/zh/audio-orchestrator-ffmpeg/bin:$PATH
+export PATH=/Users/zh/Downloads/platform-tools:$PATH
+export GPG_TTY=$(tty)
+
+[ -f ~/.gpg-agent-info ] && source ~/.gpg-agent-info
+if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
+    export GPG_AGENT_INFO
+    export SSH_AUTH_SOCK
+    export SSH_AGENT_PID
 fi
