1fc44bd9c0fe9df93a3b5eeb83345342f7780c82
ZH <zh.eden@icloud.com>
2022-12-03 02:03:30 +0800

    Update prompt.sh

--- scripts/includes/prompt.sh
+++ scripts/includes/prompt.sh
@@ -27,7 +27,7 @@ prompt-set 10 "&5%D{%a %m-%d %H:%M}&r "
 [[ "$HOST" != "ZH-MacBook-Air.local" ]] && prompt-set 10 "&gf(#55CDFC)%D{%a} &gf(#F7A8B8)%D{%m-}&f%D{%d} &gf(#F7A8B8)%D{%H:}&gf(#55CDFC)%D{%M}&r "
 # Hostname
 prompt-set 20 "&1%m&r "
-[[ "$HOST" == "ZH-MacBook-Air.local" ]] && prompt-set 20 "&gf(#55CDFC)ZH&gf(#F7A8B8)NET&fD&gf(#F7A8B8)-ORGE&gf(#55CDFC)-Control&r "
+[[ "$HOST" == "ZH-MacBook-Air.local" ]] && prompt-set 20 "&gf(#55CDFC)H&gf(#F7A8B8)y&fD&gf(#F7A8B8)E&gf(#55CDFC)V&r "
 # Username, or show a cat if I'm ZH
 prompt-set 30 "&e%n&r "
 [[ "$USER" == "zh" ]] && prompt-set 30 "🐱 "
